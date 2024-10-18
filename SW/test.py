fb9a04178ff316f102b008388775689ade0d366a
Szabo.Misi <mihaly.szabo@prodsp.hu>
2024-10-18 18:06:52 +0200

    added optional snapshot take, and time stamp

--- SW/test.py
+++ SW/test.py
@@ -11,6 +11,7 @@ buffer_size = 1024  # Number of data points to collect
 data_buffer = [0]*buffer_size
 data_lock = threading.Lock()
 stop_event = threading.Event()
+data_ready_event = threading.Event()
 
 def updatePlot():
     global buffer_size
@@ -38,7 +39,7 @@ def updatePlot():
         ax.set_ylim(1, max(data_buffer) + 100)
 
         plt.draw()                
-        plt.pause(0.1)  
+        plt.pause(1)  
         #time.sleep(1)
 
 
@@ -74,17 +75,19 @@ def SerialCom():
                 
                     if  index == buffer_size-1 :
                         #print("asd")
-                        print(f"Received {index + 1} packets...")
- 
+                        local_time = time.localtime()
+                        formatted_time = time.strftime("%H:%M:%S", local_time)
+                        print(f"[{formatted_time}] Received {index + 1} packets...")
+                        data_ready_event.set()
 
                 except ValueError:
-                    print("Cannot convert  to an integer.")
+                    #print("Cannot convert  to an integer.")
                     pass
-                except KeyboardInterrupt:
-                    print("\nExiting...")
+                #except KeyboardInterrupt:
+                #    print("\nExiting...")
 
     except serial.SerialException as e:
-        print(f"Error: Could not open {port}. {e}")
+        print(f"Error: Could not open {com_port}. {e}")
         sys.exit(1)
 
     finally:
@@ -92,27 +95,65 @@ def SerialCom():
         if 'ser' in locals() and ser.is_open:
             ser.close()
             print("Serial connection closed.")
-    
 
+def save_data_periodiccaly(name,period):
+    while not stop_event.is_set():
+
+        time.sleep(period)
+        
+        local_time = time.localtime()
+        file_name = f'{name}_snapshot_{time.strftime("%H-%M-%S", local_time)}.txt'
+        print(f"Saving to {file_name}")
+        channel_num = 0
+        with open(file_name,'w') as file:
+            for value in data_buffer:
+                channel_num = channel_num + 1
+                file.write(f'{channel_num}:{value}\n')
+        print(f"Data saved to: {file_name}")
+        #time.sleep(1)
 
 
 def main():
     # Ask for the COM port from the user
-    
-    
-    try:
-        thread_serial = threading.Thread(target=SerialCom)
-        thread_plot = threading.Thread(target=updatePlot)
-    
-        thread_plot.start()
-        thread_serial.start()
-        
-        input("Stop the data collection press enter: ")
-        stop_event.set()
 
-        thread_serial.join()
-        thread_plot.join()
+    try:
         
+        thread_serial = threading.Thread(target=SerialCom)
+        
+        save_periodically = input("Save the data periodiccaly [y/n]:")
+        save_periodically_name = ""
+        period_time = 0
+
+        if save_periodically == "y" or save_periodically == "Y":
+            period_time = 60*int(input("Give the period time in minute [min]:"))
+            save_periodically_name = input("Add name to data [name_snapshot_time]:")
+       
+       # thread_plot.start()
+        thread_period_data_saver = threading.Thread(target=save_data_periodiccaly,args=(save_periodically_name,period_time,))
+        
+        
+        if period_time != 0:
+            thread_period_data_saver.start()
+
+        thread_serial.start()
+        updatePlot()
+
+        while True:
+            if data_ready_event.is_set(): 
+                updatePlot()
+                data_ready_event.clear()
+            time.sleep(1)
+     
+
+        #thread_plot.join()
+    except KeyboardInterrupt:
+        stop_event.set()
+        thread_serial.join()
+        if period_time != 0:
+            thread_period_data_saver.join()
+        
+    finally:
+
         save_to_file = input("Save to the measured data to .txt [y/n]:")
 
         if save_to_file == "y" or save_to_file == "Y":
@@ -123,12 +164,12 @@ def main():
                 for value in data_buffer:
                     channel_num = channel_num + 1
                     file.write(f'{channel_num}:{value}\n')
-            print(f"Data saved to{file_name}")
+            print(f"Data saved to: {file_name}")
         else:
             print("Exiting from application")
-  
-    except:
-        pass
+        
+        plt.ioff()
+        plt.close()
     
 
 if __name__ == "__main__":
